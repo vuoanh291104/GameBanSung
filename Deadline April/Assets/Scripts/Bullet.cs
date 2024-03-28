0ac7c7c2517534c8f753a5ed5e685f96a6791072
Vu Oanh <nekodeath123@gmail.com>
2024-03-28 17:33:56 +0700

    add base game

---
+++ Deadline April/Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed=10f;
+    public float lifeTime =2f;
+    [SerializeField] private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rb.velocity= transform.right * speed;
+        Destroy(gameObject, lifeTime);
+    }
+
+    
+}
---
+++ Deadline April/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed= 2f;
+    [SerializeField] private Rigidbody2D rb;
+    private Vector2 moveInput;
+    [SerializeField] private Animator anim;
+    [SerializeField] private Camera mainCamera;
+    public GameObject bulletPrefabs;
+    public Transform firePoint;
+    public float fireRate= 0.5f;
+    private float nextFireTime;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput= new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        UpdateAnimation();
+        if(Input.GetMouseButtonDown(0) && Time.time >= nextFireTime){
+            Shoot();
+            nextFireTime= Time.time +fireRate;
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bulletPrefabs,firePoint.position,firePoint.rotation);
+    }
+
+    private void UpdateAnimation()
+    {
+        if(moveInput.magnitude>0){
+            anim.SetBool("IsRunning",true);
+        }else{
+            anim.SetBool("IsRunning",false);
+        }
+    }
+
+    void FixedUpdate(){
+        rb.velocity= moveInput.normalized*moveSpeed;
+        RotatePlayer();
+    }
+
+    private void RotatePlayer()
+    {
+        Vector2 mousePosition= mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDirection= mousePosition - rb.position;
+        float angle= Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+    }
+}
