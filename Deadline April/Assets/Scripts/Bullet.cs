25daf3ccfb03f3f48c07a6005dfe1ea3aca82e3e
Vu Oanh <nekodeath123@gmail.com>
2024-04-02 23:37:01 +0700

    effect

--- Deadline April/Assets/Scripts/Bullet.cs
+++ Deadline April/Assets/Scripts/Bullet.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -7,6 +9,7 @@ public class Bullet : MonoBehaviour
     public float speed=10f;
     public float lifeTime =2f;
     [SerializeField] private Rigidbody2D rb;
+    public GameObject Effect;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +17,11 @@ public class Bullet : MonoBehaviour
         
         rb.velocity= transform.right * speed;
         Destroy(gameObject, lifeTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.tag == "Enemy"){
+            Instantiate(Effect,transform.position,quaternion.identity);
+        }
     }
 
     
