b235d2547dc5769b34f5280133328b16776a73fd
Vu Oanh <nekodeath123@gmail.com>
2024-04-05 21:45:59 +0700

    high score

--- Deadline April/Assets/Scripts/GameUI.cs
+++ Deadline April/Assets/Scripts/GameUI.cs
@@ -8,6 +8,8 @@ public class GameUI : MonoBehaviour
 {
     public TextMeshProUGUI Score;
     public TextMeshProUGUI HighScore;
+    public  int currentScore;
+    public static int oldScore=0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +18,17 @@ public class GameUI : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   currentScore= GameManager.instance.GetScore();
         Score.SetText(GameManager.instance.GetScore().ToString());
-        HighScore.SetText(GameManager.instance.GetScore().ToString());
+        if(currentScore > oldScore){
+            oldScore = currentScore;
+            HighScore.SetText(oldScore.ToString());
+
+        }else{
+            HighScore.SetText(oldScore.ToString());
+
+        }
+        
     }
 
     public void Menu(){
